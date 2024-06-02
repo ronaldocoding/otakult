64058be33637a1bb66ede2901e37764a3fbd985c
ronaldocosta <ronaldocosta.developer@gmail.com>
2024-06-02 18:26:49 -0400

    Add basic necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,21 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hiltAndroid)
 }
 
 android {
     namespace = "br.com.otakult"
     compileSdk = 34
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
 
     defaultConfig {
         applicationId = "br.com.otakult"
@@ -38,10 +48,41 @@ android {
 
 dependencies {
 
+    // Coil
+    implementation(libs.coil)
+
+    // Core
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Compose
+    implementation(libs.compose.ui)
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.animation)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material3)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compile)
+    ksp(libs.hilt.compiler)
+
+    // Mockk
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
+
+    // Okhttp
+    implementation(libs.okhttp)
+
+
+    // Retrofit
+    implementation(libs.retrofit)
+
+    // Turbine
+    testImplementation(libs.turbine)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.hiltAndroid) apply false
+    alias(libs.plugins.ksp)
 }
 true // Needed to make the Suppress annotation work for the plugins block
